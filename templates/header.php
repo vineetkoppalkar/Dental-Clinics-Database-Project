a4320c8da6ded8c99d02cc8af6c708628b20ce82
Vineet <koppalkarvineet@gmail.com>
2020-03-24 11:23:58 -0400

    Initial bootstrap setup

---
+++ templates/header.php
@@ -0,0 +1,16 @@
+<div class="header clearfix my-4">
+  <nav>
+    <ul class="nav nav-pills float-right">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </nav>
+  <h3 class="text-muted">COMP 353</h3>
+</div>
