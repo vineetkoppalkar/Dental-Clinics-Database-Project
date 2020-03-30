4d018c59c17e3cd0bca8259501842c19cae2a5ac
Mstc08 <minjun_TC@hotmail.com>
2020-03-30 17:28:20 -0400

    Refactored treatment files name and implemented displaying dentists and more details about their appointments

---
+++ database/dentists/getDentists.php
@@ -0,0 +1,30 @@
+<div class="row">
+    <?php
+    $conn = OpenCon();
+
+    $sql = "SELECT * FROM Professionals WHERE isDentist = True";
+    $result = $conn->query($sql);
+
+    $todayDate = date("Y-m-d");
+
+    if ($result->num_rows > 0) {
+        while ($dentists = $result->fetch_assoc()) {
+            echo '<div class="col-sm-4 my-3">' .
+                '<div class="card">' .
+                '<div class="card-body">' .
+                '<h5 class="card-title">' . $dentists['name'] . '</h5>' .
+                '<p class="card-text">' . $dentists['phoneNumber']  . '</p>' .
+                '<a href="/templates/dentists/dentist.php?professionalId=' . $dentists['professionalId'] . '&date=' . $todayDate .
+                '" class="btn btn-outline-primary">Details</a>' .
+                '</div>' .
+                '</div>' .
+                '</div>';
+        }
+    } else {
+        echo "0 dentists found";
+    }
+
+    CloseCon($conn);
+    ?>
+
+</div>
