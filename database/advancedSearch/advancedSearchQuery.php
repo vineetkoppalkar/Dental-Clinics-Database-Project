554a75f90dfc037dfd6ca2d0be9ac0f68c5c486c
PetiaPav <petia2397@gmail.com>
2020-04-04 17:22:39 -0400

    Improved error handling

--- database/advancedSearch/advancedSearchQuery.php
+++ database/advancedSearch/advancedSearchQuery.php
@@ -32,67 +32,61 @@
 
       echo "<h4>Query Result: </h4>";
      
-      if(strpos(strtolower($sqlQuery), "select") !== false){
-         //Add check to see if the table selected is valid
-        $validTables = array("Appointments", "Bills", "DentalClinics", "Patients", "PerformedAt", "PerformedBy", "PreparedBy", "ProfessionalAt", "Professionals", "ReceivedBy", "ReceptionistAt", "Receptionists", "TreatmentPerformed", "Treatments");
-        $validTableName = false;
-        
-        foreach($validTables as $tableName){
-          if(strpos($sqlQuery, $tableName) !== false){
-            $validTableName = true;
-            break;
-          }
+      //Add check to see if the table selected is valid
+      $validTables = array("Appointments", "Bills", "DentalClinics", "Patients", "PerformedAt", "PerformedBy", "PreparedBy", "ProfessionalAt", "Professionals", "ReceivedBy", "ReceptionistAt", "Receptionists", "TreatmentPerformed", "Treatments");
+      $validTableName = false;
+      
+      foreach($validTables as $tableName){
+        if(strpos($sqlQuery, $tableName) !== false){
+          $validTableName = true;
+          break;
         }
-        
-        if($validTableName == false){
-          echo 'The entered table name does not exist. </br></br>' .
-            '<div class="card">
-              <div class="card-header">
-                Tables
-              </div>
-              <div class="card-body">
-              <p class="card-text">Appointments, Bills, DentalClinics, Patients, PerformedAt, PerformedBy, PreparedBy, ProfessionalAt, Professionals, ReceivedBy, ReceptionistAt, Receptionists, TreatmentPerformed, Treatments</p>
-              </div
-            </div>';
-        // }else if(!is_object($result){
-        //   echo 'error';
-        }else if($result->num_rows > 0) {
-          $firstRow = $result->fetch_assoc();
+      }
+    
+      if($validTableName == false){
+        echo 'The entered table name does not exist. </br></br>' .
+          '<div class="card">
+            <div class="card-header">
+              Tables
+            </div>
+            <div class="card-body">
+            <p class="card-text">Appointments, Bills, DentalClinics, Patients, PerformedAt, PerformedBy, PreparedBy, ProfessionalAt, Professionals, ReceivedBy, ReceptionistAt, Receptionists, TreatmentPerformed, Treatments</p>
+            </div
+          </div>';
+      }else if(is_object($result) && $result->num_rows > 0) {
+        $firstRow = $result->fetch_assoc();
+        echo '<li class="w-100 list-group-item container">' .
+        '<div class="row">';
+        foreach($firstRow as $column=>$value){
+          echo '<div class="col-sm text-center font-weight-bold">' .
+          $column .
+          '</div>'; 
+        }
+        echo '</div>' .
+        '<div class="row">';
+        foreach($firstRow as $column=>$value){
+          echo '<div class="col-sm text-center">' .
+          $value .
+          '</div>'; 
+        }
+        echo '</div>';
+        while($result_row = $result->fetch_assoc()){
           echo '<li class="w-100 list-group-item container">' .
           '<div class="row">';
-          foreach($firstRow as $column=>$value){
-            echo '<div class="col-sm text-center font-weight-bold">' .
-            $column .
-            '</div>'; 
-          }
-          echo '</div>' .
-          '<div class="row">';
-          foreach($firstRow as $column=>$value){
+          foreach($result_row as $attribute=>$attribute_value) {
             echo '<div class="col-sm text-center">' .
-            $value .
+            $attribute_value .
             '</div>'; 
           }
           echo '</div>';
-          while($result_row = $result->fetch_assoc()){
-            echo '<li class="w-100 list-group-item container">' .
-            '<div class="row">';
-            foreach($result_row as $attribute=>$attribute_value) {
-              echo '<div class="col-sm text-center">' .
-              $attribute_value .
-              '</div>'; 
-            }
-            echo '</div>';
-          }
-          echo '</div>';
-        }else{
-          echo "0 Results Found.";
         }
-      }else{
-        if(!$result){
-          echo 'Error while running this query. </br></br>';
-        } else if($result == 1){
-          echo 'The query has executed successfully! </br></br>';
-        } 
+        echo '</div>';
+      }else if(!$result){
+        echo 'Error while running this query. </br></br>';
+      } else if($result == 1){
+        echo 'The query has executed successfully! </br></br>';
+      } else if($result == 0){
+        echo '0 results found. </br></br>';
       }
 
       CloseCon($conn);
