ff5673cb8d28f8a012bca9ca78aa0cbfb00eecb4
Vineet <koppalkarvineet@gmail.com>
2020-04-05 16:11:34 -0400

    Implemented new bills when new appointments are created

--- database/appointments/deleteAppointment.php
+++ database/appointments/deleteAppointment.php
@@ -6,7 +6,17 @@
 
   $conn = OpenCon();
 
-  $sql = "DELETE FROM Appointments WHERE appointmentId = $appointmentId";
+  $sql = "SELECT  billId
+          FROM    TreatmentPerformed
+          WHERE   appointmentId = $appointmentId
+          GROUP BY appointmentId;";
+  $result = $conn->query($sql);
+  $appointmentBillId = intval($result->fetch_assoc()['billId']);
+
+  $sql = "DELETE FROM Appointments WHERE appointmentId = $appointmentId;";
+  $result = $conn->query($sql);
+
+  $sql = "DELETE FROM Bills WHERE billId = $appointmentBillId;";
   $result = $conn->query($sql);
 
   CloseCon($conn);
