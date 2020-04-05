677121953bef9af4318e8db9c9014f9e0b67f64a
Vineet <koppalkarvineet@gmail.com>
2020-04-05 17:15:56 -0400

    Fixed date and time update and added bill cost update

--- database/appointments/updateAppointment.php
+++ database/appointments/updateAppointment.php
@@ -10,7 +10,7 @@
 
   $conn = OpenCon();
 
-  $sql = "UPDATE Appointments SET date = $date, time = $time WHERE appointmentId = $appointmentId;";
+  $sql = "UPDATE Appointments SET date = '$date', time = '$time' WHERE appointmentId = $appointmentId;";
   $result = $conn->query($sql);
 
   $sql = "UPDATE PerformedAt SET clinicId = $clinicId WHERE appointmentId = $appointmentId;";
@@ -19,13 +19,29 @@
   $sql = "UPDATE PerformedBy set professionalId = $professionalId WHERE appointmentId = $appointmentId;";
   $result = $conn->query($sql);
 
+  $sql = "SELECT  billId
+          FROM    TreatmentPerformed
+          WHERE   appointmentId = $appointmentId
+          GROUP BY appointmentId;";
+  $result = $conn->query($sql);
+  $billId = intval($result->fetch_assoc()['billId']);
+
   $sql = "DELETE FROM TreatmentPerformed WHERE appointmentId = $appointmentId;";
   $result = $conn->query($sql);
 
+  $totalAppointmentCost = 0;
   foreach ($treatmentIds as $treatmentId) {
-    $sql = "INSERT INTO TreatmentPerformed (appointmentId, treatmentId) VALUES ($appointmentId, $treatmentId);";
+    $sql = "SELECT cost FROM Treatments WHERE treatmentId = $treatmentId";
+    $result = $conn->query($sql);
+    $treatmentCost = $result->fetch_assoc()['cost'];
+    $totalAppointmentCost += $treatmentCost;
+    
+    $sql = "INSERT INTO TreatmentPerformed (appointmentId, billId, treatmentId) VALUES ($appointmentId, $billId, $treatmentId);";
     $result = $conn->query($sql);
   }
-  
+
+  $sql = "UPDATE Bills set amount = $totalAppointmentCost WHERE billId = $billId;";
+  $result = $conn->query($sql);
+    
   CloseCon($conn);
 ?>
