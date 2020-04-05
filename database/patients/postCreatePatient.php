f1ee603cb4aec2280fb83d74c1993523bda94f02
prich28 <paulrichard.28@gmail.com>
2020-04-05 11:00:34 -0400

    fixed add patient

--- database/patients/postCreatePatient.php
+++ database/patients/postCreatePatient.php
@@ -6,10 +6,11 @@
     $name = $_POST['createName'];
     $address = $_POST['createAddress'];
     $phoneNumber = $_POST['createPhoneNumber'];
-
+    
+    $conn = OpenCon();
     $sql = "INSERT INTO Patients (name, address, phoneNumber) VALUES ('$name', '$address', '$phoneNumber');";
     $result = $conn->query($sql);
-    
+
     CloseCon($conn);
     
 ?>
