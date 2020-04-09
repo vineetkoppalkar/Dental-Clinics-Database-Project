117e6f8872db926c71dc93ab956479202265bb94
PetiaPav <petia2397@gmail.com>
2020-04-09 17:39:58 -0400

    changing path

--- templates/menuOptions.php
+++ templates/menuOptions.php
@@ -1,31 +1,31 @@
 <div class="container ">
   <div class="row justify-content-center row-cols-2">
-    <a href="/templates/clinics/clinics.php" class="col text-center">
+    <a href="./clinics/clinics.php" class="col text-center">
       <div class="mainMenuItems">
         CLINICS
       </div>
     </a>
-    <a href="/templates/dentists/dentists.php" class="col text-center">
+    <a href="./dentists/dentists.php" class="col text-center">
       <div class="mainMenuItems">
         DENTISTS
       </div>
     </a>
-    <a href="/templates/patients/patients.php" class="col text-center">
+    <a href="./patients/patients.php" class="col text-center">
       <div class="mainMenuItems">
         PATIENTS
       </div>
     </a>
-    <a href="/templates/appointments/appointments.php" class="col text-center">
+    <a href="./appointments/appointments.php" class="col text-center">
       <div class="mainMenuItems">
         APPOINTMENTS
       </div>
     </a>
-    <a href="/templates/bills/bills.php" class="col text-center">
+    <a href="./bills/bills.php" class="col text-center">
       <div class="mainMenuItems">
         BILLS
       </div>
     </a>
-    <a href="/templates/treatments/treatments.php" class="col text-center">
+    <a href="./treatments/treatments.php" class="col text-center">
       <div class="mainMenuItems">
         TREATMENTS
       </div>
