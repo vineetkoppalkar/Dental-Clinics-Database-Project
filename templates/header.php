e08132a4553979e15249dde6ce0d2ac70065b356
PetiaPav <petia2397@gmail.com>
2020-04-09 19:41:27 -0400

    advanced search fix

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,7 @@
         <a class="nav-link active" href="../../index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="templates/advancedSearch/advancedSearch.php">Advanced Search</a>
+        <a class="nav-link" href="https://fvc353.encs.concordia.ca/templates/advancedSearch/advancedSearch.php">Advanced Search</a>
       </li>
     </ul>
   </nav>
